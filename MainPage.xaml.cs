38d764759417b3704f92bfe7735f1629cdac9404
Lukas Baron <lukas.baron@tngtech.com>
2024-03-13 10:09:09 +0100

    Initial project, created in Visual Studio 2022 as a "Blazor Hybrid" project

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToolbeltHotkeys2Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ToolbeltHotkeys2Demo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
